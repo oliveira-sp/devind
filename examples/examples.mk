57a6e70f0cab7b954f271254e2383912a47cfa62
Stéphane oliveira <oliveirapst@gmail.com>
2025-06-08 14:07:14 +0200

    Add examples list target

--- examples/examples.mk
+++ examples/examples.mk
@@ -1,9 +1,18 @@
-DEVIND := $(realpath src/devind)
+EXAMPLES := $(notdir $(shell find examples -mindepth 1 -maxdepth 1 -type d))
 
 .PHONY: run-example-%
 run-example-%: ## Run example by folder name
 	$(QUIET)./$(DEVIND_SCRIPT) \
 		DEVIND_MAKEFILE_ENTRY=examples/$*/Makefile \
 		DEVIND_YAML_FILE=examples/$*/devind.yaml \
-		YAML_TOOL=src/devind_yaml_parser.awk \
-		$(GOALS)
+		YAML_TOOL=src/devind_yaml_parser.awk $(GOALS)
+
+.PHONY: list-examples
+list-examples: ## List the available examples and show usage
+	$(QUIET)echo "Usage: make run-example-<name> GOALS=your_goal"
+	$(QUIET)echo
+	$(QUIET)echo "Available examples:"
+	$(QUIET)for d in $(EXAMPLES); do echo " - $$d"; done
+	$(QUIET)echo
+	$(QUIET)echo "Example:"
+	$(QUIET)echo "  make run-example-$(firstword $(EXAMPLES)) GOALS=clean"
