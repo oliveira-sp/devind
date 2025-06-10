a31c4145c094e1d472b7a1f64da34ce05210eb2e
Stéphane oliveira <oliveirapst@gmail.com>
2025-06-11 00:00:59 +0200

    Create unit-test related makefile

---
+++ test/test.mk
@@ -0,0 +1,23 @@
+# Unit-testing related targets ================================================
+TEST_IMAGE_NAME := devind-test:latest
+TEST_IMAGE_STAMP := test/.devind-test.built
+TEST_DOCKERFILE := test/Dockerfile
+TEST_CONTEXT := test
+
+.PHONY: test
+test: build-test-image ## Execute DevinD unit-tests
+	$(QUIET)docker run --rm -it -v "${PWD}:/code" $(TEST_IMAGE_NAME) test
+
+$(TEST_IMAGE_STAMP): $(TEST_DOCKERFILE)
+	@echo "[+] Building $(TEST_IMAGE_NAME)..."
+	@docker build -t $(TEST_IMAGE_NAME) -f $(TEST_DOCKERFILE) $(TEST_CONTEXT)
+	@touch $(TEST_IMAGE_STAMP)
+
+.PHONY: build-test-image
+build-test-image: $(TEST_IMAGE_STAMP) ## Build image only if stamp is missing or Dockerfile changed
+
+.PHONY: clean-test-image
+clean-test-image: ## Clean test image
+	@echo "[~] Removing $(TEST_IMAGE_NAME) image and build stamp..."
+	@docker rmi $(TEST_IMAGE_NAME) || true
+	@rm -f $(TEST_IMAGE_STAMP)
